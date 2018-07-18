a3262d82ccb554cf37f9f5e91047acce7d022120
schahnaz <schahnaz@mail.de>
2018-07-18 16:27:34 +0200

    finished

---
+++ README.md
@@ -0,0 +1,5 @@
+doctrine_example
+================
+
+A Symfony project created on July 18, 2018, 3:50 pm.
+# FSWD40-day03-symfony
---
+++ src/AppBundle/Entity/Example_class.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+
+* @ORM\Entity
+
+* @ORM\Table(name="examples")
+
+*/
+
+class Example_class
+
+{
+
+ /**
+
+ * @ORM\Column(type="integer")
+
+ * @ORM\Id
+
+ * @ORM\GeneratedValue(strategy="AUTO")
+
+ */
+
+ private $id;
+
+ /**
+
+ * @ORM\Column(type="string", length=50)
+
+ */
+
+ private $name;
+
+ /**
+
+ * @ORM\Column(type="text")
+
+ */
+
+ private $address;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Example_class
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     *
+     * @return Example_class
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+}
